6568f59815804056217eef03e683f1fb86660bd6
cezarbuna <cezar.buna25@gmail.com>
2022-07-31 15:25:03 +0300

    added delete wallet command for convenience

---
+++ CryptoAvenue/CryptoAvenue.Application/Commands/WalletCommands/DeleteWalletCommand.cs
@@ -0,0 +1,15 @@
+﻿using CryptoAvenue.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoAvenue.Application.Commands.WalletCommands
+{
+    public class DeleteWalletCommand : IRequest<Wallet>
+    {
+        public Guid WalletId { get; set; }
+    }
+}
